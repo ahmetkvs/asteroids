7ed1ee27a8657add23c9dad75cfdd7bc2f75f53d
ahmetkvs <ahmetpkavas@gmail.com>
2025-07-27 16:49:59 +0300

    Created player class and override the circlesphapes draw method for it

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 #from constants import * # This lets you use the variable, method, class names directly
 import constants # with this you have to prefix everything with contants.
+import player as playerLib
 
 def main():
     print("Starting Asteroids!")
     numpass, numfail = pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = playerLib.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         #Delta time is calculated (/1000 is for miliseconds)
