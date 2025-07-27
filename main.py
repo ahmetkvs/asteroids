7d6b2ebdeda18777a3c375b6d1b4559799d5884c
ahmetkvs <ahmetpkavas@gmail.com>
2025-07-27 16:40:25 +0300

    Created circleshape sprite class

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ def main():
     print("Starting Asteroids!")
     numpass, numfail = pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    game_on = True
-    while(game_on):
+    clock = pygame.time.Clock()
+    dt = 0
+    while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        #Delta time is calculated (/1000 is for miliseconds)
+        dt = clock.tick(60) / 1000
     
     
 
